39adb99495f2a176311116e3277261ff76c8ee06
Jeckill1993 <evgeniyduplenko@gmail.com>
2022-09-17 02:32:06 +0300

    [main] add database connection

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const http = require('http');
+const mysql = require('mysql');
 
 const hostname = '127.0.0.1';
 const port = 5000;
+
+const database = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "roze"
+});
+
+database.connect((error) => {
+    if (error) throw error;
+    console.log("Connected!");
+    const sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+
+    database.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
+});
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
